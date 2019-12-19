05ed6bc7b0172bec5b1ec5421eac8e3ab2d1c51e
Евгений <gekachan2000@gmail.com>
2019-12-19 09:28:27 +0500

    init project

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+// import * as path from "path";
+// import cookieParser from "cookie-parser";
+// import bodyParser from "body-parser";
+
+const rootDir = process.cwd();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.static('static'));
+
+app.get("/", (_, res) => {
+    res.sendFile(path.join(rootDir, "static/index.html"));
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
