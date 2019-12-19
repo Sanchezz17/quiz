3eb51927dacc3b67018850fc07e83173358902db
Евгений <gekachan2000@gmail.com>
2019-12-19 10:37:41 +0500

    Добавлены картинки к вопросам

--- server.js
+++ server.js
@@ -18,11 +18,13 @@ app.get("/", (_, res) => {
 const questions = [
     {
         name: "Какой тигр самый крупный?",
+        imageURL: "https://images.wallpaperscraft.com/image/tiger_aggression_teeth_predator_big_cat_106887_1600x1200.jpg",
         options: ["Амурский", "Малазийский", "Индийский", "Суматранский"],
         answer: ["Индийский"]
     },
     {
         name: "Где живет коала?",
+        imageURL: "https://versiya.info/uploads/posts/2019-02/1550993623_s1200.jpg",
         options: [
             "В горной пещере",
             "В бамбуковом лесу",
@@ -33,11 +35,13 @@ const questions = [
     },
     {
         name: "Какого цвета хвост у зебры?",
+        imageURL: "http://fullhdwallpapers.ru/image/animals/2852/tri-zebry.jpg",
         options: ["Белый", "Черный", "Серый", "Коричневый"],
         answer: ["Черный"]
     },
     {
         name: "Какое животное самое быстрое?",
+        imageURL: "https://s1.1zoom.ru/big3/25/Cheetahs_Cubs_Two_Grass_527375_2048x1152.jpg",
         options: ["Лев", "Зебра", "Сапсан", "Гепард"],
         answer: ["Гепард"]
     },
